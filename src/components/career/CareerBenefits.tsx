1bedcbc3b49cad55ab5318618afe8310ad2ba077
yogiannanf <yogiannanf@gmail.com>
2025-11-27 11:36:41 +0700

    add career

---
+++ src/components/career/CareerBenefits.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export function CareerBenefits() {
+  return (
+    <section className="py-16 lg:py-24 bg-white">
+      <div className="max-w-[1400px] mx-auto px-4 lg:px-6">
+        <h2 className="text-3xl lg:text-4xl font-bold mb-12">Career Benefits</h2>
+        {/* Content will be added here */}
+      </div>
+    </section>
+  );
+}
---
+++ src/components/career/JobOpenings.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export function JobOpenings() {
+  return (
+    <section className="py-16 lg:py-24 bg-gray-50">
+      <div className="max-w-[1400px] mx-auto px-4 lg:px-6">
+        <h2 className="text-3xl lg:text-4xl font-bold mb-12">Job Openings</h2>
+        {/* Content will be added here */}
+      </div>
+    </section>
+  );
+}
